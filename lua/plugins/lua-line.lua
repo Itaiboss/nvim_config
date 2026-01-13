2cf79b8f87ee2a2d5139b349983990a57eefff95
Itai Boss <itaiboss@Itais-MacBook-Pro.local>
2026-01-12 21:51:32 -0800

    splash test

--- lua/plugins/lua-line.lua
+++ lua/plugins/lua-line.lua
@@ -1,11 +1,11 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    
+    "nvim-lualine/lualine.nvim",
+
     config = function()
-        require('lualine').setup({
-            options ={
-                theme = 'kanagawa'
-            }
+        require("lualine").setup({
+            options = {
+                theme = "kanagawa",
+            },
         })
-    end
+    end,
 }
